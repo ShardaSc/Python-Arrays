2b1dae4f0de211dc28b5b6a16adc7b421a87c667
Sharda <shardachawre96@gmail.com>
2025-03-08 22:49:09 +0530

    Weather Palindrome or not

---
+++ Python Program to Check if a String is Palindrome or Not.py
@@ -0,0 +1,18 @@
+# s = "121"  # string
+s = "malayalam"
+i,j = 0, len(s) - 1
+is_palindrome = True
+while i < j:
+    if s[i] != s[j]:
+        is_palindrome = False
+        break
+    i += 1
+    j -= 1
+
+
+if is_palindrome:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
