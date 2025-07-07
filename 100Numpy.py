51b93fc211ec2b0a4cca212330d330e70c36aa50
Sharda <shardachawre96@gmail.com>
2025-07-07 23:15:03 +0530

    100 Numpy examples

--- 100Numpy.py
+++ 100Numpy.py
@@ -10,3 +10,11 @@ print(np.__version__)
 
 #3. Create a null vector of size 10 (★☆☆)
 print(np.zeros(10))
+
+#4. How to find the memory size of any array (★☆☆)
+
+arr = np.zeros(10)
+print(arr.nbytes)
+
+#5. How to get the documentation of the numpy add function from the command line? (★☆☆)
+help(np.add)
