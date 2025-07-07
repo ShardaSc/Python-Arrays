c6e1d730531745a12b1b2e4fe2f7927c956cb39e
Sharda <shardachawre96@gmail.com>
2025-07-07 22:58:51 +0530

    100 Numpy examples

---
+++ 100Numpy.py
@@ -0,0 +1,12 @@
+# 100 numpy exercises
+
+#1. Import the numpy package under the name `np` (★☆☆)
+
+import numpy as np
+
+#2. Print the numpy version and the configuration (★☆☆)
+print(np.__version__)
+
+
+#3. Create a null vector of size 10 (★☆☆)
+print(np.zeros(10))
