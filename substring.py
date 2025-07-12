1917d72ca15f77155d38dca562b6cac8c2dac2d7
Sharda <shardachawre96@gmail.com>
2025-07-12 23:08:21 +0530

    Substring  length without repeating characters

--- substring.py
+++ substring.py
@@ -31,8 +31,21 @@ def longest_unique_substring_length(s):
 
     return max_len
 
+#Using Set and Two Pointers
+def longest_unique_substring_set(s):
+    seen = set()
+    left = 0
+    max_len = 0
 
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+        seen.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
 
 s = "abcabcbb"
 print(longest_unique_substring_length(s))
-# print(longest_unique_substring_set(s))
+print(longest_unique_substring_set(s))
